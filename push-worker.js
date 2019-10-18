9035fccbd320256770af2921bf684349a97e6c34
Carlos Cruz <carlos.cruz@caliente.mx>
2019-10-18 16:19:16 -0700

    Test airship implementation

---
+++ push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1571355915.209663
+importScripts('https://aswpsdkus.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://s3.amazonaws.com/com.urbanairship.appicons/5rxw2eyERsOqNj7gur3w_w/icon.PNG?1571351348',
+  defaultTitle: 'Caliente Sports',
+  defaultActionURL: 'https://sports.caliente.mx',
+  appKey: '5rxw2eyERsOqNj7gur3w_w',
+  token: 'MTo1cnh3MmV5RVJzT3FOajdndXIzd193OmF2bkRNbU1Xb3FwTlZ1Nk1vYzZ2T2N1TmdYQ0FQMkwtNWRSNWxkVGtCRTg',
+  vapidPublicKey: 'BENlhaWmotBwqc2LV2vs_YM0Srbm8_ZZDdipp-imcHpcYojJEEKg8XTbN3PpfrPsKb7OQ4P3aSGl84MbKrIR91Q='
+})
